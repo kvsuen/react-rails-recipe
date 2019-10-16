92d0db004fecea596cb00e41c02b33a551125759
Kevin Suen <kevinsuen11@gmail.com>
2019-10-15 20:13:35 -0400

    Implement recipe view

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home/home.component";
 import Recipes from "../components/Recipes/recipes.component";
+import Recipe from "../components/Recipes/Recipe/recipe.component";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/recipes" exact component={Recipes} />
+      <Route path="/recipe/:id" exact component={Recipe} />
     </Switch>
   </Router>
 );
