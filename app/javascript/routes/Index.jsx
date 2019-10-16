96c444ca523c3d19273bfbd4cb4c7050f1dc7caf
Kevin Suen <kevinsuen11@gmail.com>
2019-10-15 20:27:48 -0400

    Implement create new recipe view

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home/home.component";
 import Recipes from "../components/Recipes/recipes.component";
 import Recipe from "../components/Recipes/Recipe/recipe.component";
+import NewRecipe from "../components/NewRecipe/new-recipe.component";
 
 export default (
   <Router>
@@ -10,6 +11,7 @@ export default (
       <Route path="/" exact component={Home} />
       <Route path="/recipes" exact component={Recipes} />
       <Route path="/recipe/:id" exact component={Recipe} />
+      <Route path="/recipe" exact component={NewRecipe} />
     </Switch>
   </Router>
 );
